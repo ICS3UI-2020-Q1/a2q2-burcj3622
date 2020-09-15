cef7a0924703d4466a9546d0eddcfa42fcd9d44a
burcj3622 <burcj3622@wrdsb.ca>
2020-09-15 13:52:48 +0000

    less than or greater than

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *lets the user knoe which number is greater
+ * @author jason
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // asks user for input
+    System.out.println("Please enter an integer:");
     
+    //lets user input number
+    int number = input.nextInt();
+
+    //asks user for second number 
+    System.out.println("Please enter another integer:");
+
+    //lets user input second number 
+    int number2 = input.nextInt();
+
+    if(number > number2)
+    System.out.println( number +" greater than " + number2);
+    else 
+    System.out.println(number+ " less than " + number2);
+
   }
 }
